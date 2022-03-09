4e21a45b8ee0d52e5da45f3dc57449c84991ad74
christhetree <christhetree@gmail.com>
2022-03-09 18:55:34 +0900

    [cm] Fix hop_length bug

--- python/realtime_stft.py
+++ python/realtime_stft.py
@@ -5,8 +5,7 @@ import numpy as np
 from torch import Tensor as T
 import torch as tr
 from torch import nn
-from torchaudio.transforms import Spectrogram
-import matplotlib.pyplot as plt
+from torchaudio.transforms import Spectrogram, InverseSpectrogram
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
@@ -26,7 +25,7 @@ class RealtimeSTFT(nn.Module):
         assert io_n_samples >= hop_length
         assert io_n_samples % hop_length == 0
         assert n_fft % 2 == 0
-        assert n_fft % hop_length == 0
+        assert (n_fft // 2) % hop_length == 0
         self.batch_size = batch_size
         self.io_n_samples = io_n_samples
         self.n_fft = n_fft
@@ -35,9 +34,9 @@ class RealtimeSTFT(nn.Module):
         self.crossfade_n_frames = crossfade_n_frames
         self.eps = eps
 
-        self._io_n_frames = io_n_samples // hop_length
-        self._overlap = self.n_fft // 2
-        self._overlap_n_frames = self._overlap // self.hop_length
+        self.io_n_frames = self.io_n_samples // self.hop_length
+        self.overlap_n_frames = self.n_fft // 2 // self.hop_length
+        self.n_bins = (self.n_fft // 2) + 1
 
         self.stft = Spectrogram(n_fft=self.n_fft,
                                 hop_length=self.hop_length,
@@ -46,58 +45,81 @@ class RealtimeSTFT(nn.Module):
                                 normalized=False,
                                 power=2.0,
                                 return_complex=True)
-        self._in_buf = None
-        self._tmp_in_buf = None
-        self._frames_buf = None
-        self._tmp_frames_buf = None
+        self.istft = InverseSpectrogram(n_fft=self.n_fft,
+                                        hop_length=self.hop_length,
+                                        pad=0,
+                                        center=True,
+                                        normalized=False)
+        self.in_buf = None
+        self.tmp_in_buf = None  # Prevents memory allocation
+        self.frames_buf = None
+        self.tmp_frames_buf = None  # Prevents memory allocation
         self.reset()
 
     def reset(self) -> None:
-        self._in_buf = tr.full(
-            (self.batch_size, self.n_fft + self.io_n_samples),
+        self.in_buf = tr.full(
+            (self.batch_size, (((self.overlap_n_frames * 2) - 1) * self.hop_length) + self.io_n_samples),
             self.eps,
         )
-        self._tmp_in_buf = tr.clone(self._in_buf)
-        # self._frames_buf = self.stft(tr.full(
-        #     (self.batch_size, (self.model_io_n_frames * self.hop_length) - 1),
-        #     self.eps)
-        # )
-        self._frames_buf = tr.zeros((self.batch_size, 5, self.model_io_n_frames))
-        self._tmp_frames_buf = tr.clone(self._frames_buf)
+        self.tmp_in_buf = tr.clone(self.in_buf)
+        self.frames_buf = self.stft(tr.full(
+            (self.batch_size, (self.model_io_n_frames * self.hop_length) - 1),
+            self.eps)
+        )
+        self.tmp_frames_buf = tr.clone(self.frames_buf)
 
-    def audio_to_spectrogram(self, audio: T) -> T:
+    def audio_to_spec(self, audio: T) -> T:
         assert audio.shape == (self.batch_size, self.io_n_samples)
-        self._tmp_in_buf[:, :-self.io_n_samples] = self._in_buf[:, self.io_n_samples:]
-        self._in_buf[:, :-self.io_n_samples] = self._tmp_in_buf[:, :-self.io_n_samples]
-        self._in_buf[:, -self.io_n_samples:] = audio
-        print(f'in_buf = {self._in_buf}')
-        frames = self.stft(self._in_buf)
-        frames = frames[:, :, self._overlap_n_frames:]
-        fixed_prev_frames = frames[:, :, :-self._io_n_frames]
-        n_fixed_prev_frames = fixed_prev_frames.shape[2]
-        new_frames = frames[:, :, -self._io_n_frames:]
-        self._frames_buf[:, :, -n_fixed_prev_frames:] = fixed_prev_frames
-        self._tmp_frames_buf[:, :, :-self._io_n_frames] = self._frames_buf[:, :, self._io_n_frames:]
-        self._frames_buf[:, :, :-self._io_n_frames] = self._tmp_frames_buf[:, :, :-self._io_n_frames]
-        self._frames_buf[:, :, -self._io_n_frames:] = new_frames
-        print(f'frames_buf = {self._frames_buf[:, -1, :]}')
-        return self._frames_buf
+        # Shift buffer left and insert audio chunk without allocating memory
+        self.tmp_in_buf[:, :-self.io_n_samples] = self.in_buf[:, self.io_n_samples:]
+        self.in_buf[:, :-self.io_n_samples] = self.tmp_in_buf[:, :-self.io_n_samples]
+        self.in_buf[:, -self.io_n_samples:] = audio
+        frames = self.stft(self.in_buf)
+        # Remove overlap frames we have computed before
+        frames = frames[:, :, self.overlap_n_frames:]
+        # Identify frames that are more correct due to missing prev audio info
+        fixed_prev_frames = frames[:, :, :-self.io_n_frames]
+        assert fixed_prev_frames.shape[2] == self.overlap_n_frames
+        # Identify the new frames for the input audio chunk
+        new_frames = frames[:, :, -self.io_n_frames:]
+        # Overwrite previous frames with more correct frames
+        self.frames_buf[:, :, -self.overlap_n_frames:] = fixed_prev_frames
+        # Shift buffer left and insert new frames without allocating memory
+        self.tmp_frames_buf[:, :, :-self.io_n_frames] = self.frames_buf[:, :, self.io_n_frames:]
+        self.frames_buf[:, :, :-self.io_n_frames] = self.tmp_frames_buf[:, :, :-self.io_n_frames]
+        self.frames_buf[:, :, -self.io_n_frames:] = new_frames
+        return self.frames_buf
+
+    def spec_to_audio(self, spec: T) -> T:
+        assert spec.shape == (self.batch_size,
+                              self.n_bins,
+                              self.model_io_n_frames)
+        # spec = spec[:, :, -(self._overlap_n_frames + self._io_n_frames):]
+        spec = spec[:, :, -5:]
+        rec_audio = self.istft(spec)
+        return rec_audio
 
     def forward(self, audio: T) -> T:
-        return self.audio_to_spectrogram(audio)
+        spec = self.audio_to_spec(audio)
+        return spec
+        # rec_audio = self.spec_to_audio(spec)
+        # derp = 1
 
 
 if __name__ == '__main__':
     batch_size = 1
-    io_n_samples = 2
-    n_fft = 8
-    hop_length = 2
-    model_io_n_frames = 19
-    audio_n_frames = 23
+    hop_length = 512
+    io_n_samples = 512
+    n_fft = 2048
+    model_io_n_frames = 16
+    audio_n_frames = 16
 
     audio = tr.rand((batch_size, hop_length * audio_n_frames))
+    # print(f'audio = {audio}')
     rts = RealtimeSTFT(
         batch_size, io_n_samples, n_fft, hop_length, model_io_n_frames)
+    # print(f'rts.overlap_n_frames = {rts.overlap_n_frames}')
+    assert audio_n_frames % rts.io_n_frames == 0
 
     all_at_once = rts.stft(audio)
     chunked_out = None
@@ -110,15 +132,20 @@ if __name__ == '__main__':
 
     all_np = all_at_once.numpy()[0]
     chunked_np = chunked_out.numpy()[0]
-    all_np = all_np[:, -model_io_n_frames:]
-    # exit()
-    print(np.allclose(all_np, chunked_np))
-    print(np.allclose(all_np[:, 1:], chunked_np[:, 1:]))
 
-    plt.imshow(all_np)
-    plt.title('all_np')
-    plt.show()
-    plt.imshow(chunked_np)
-    plt.title('chunked_np')
-    plt.show()
-    derp = 1
+    cut_idx = rts.overlap_n_frames - 1
+    cut_chunked_np = chunked_np[:, cut_idx:]
+    all_np = all_np[:, -model_io_n_frames:]
+    cut_all_np = all_np[:, cut_idx:]
+    print(np.allclose(all_np, chunked_np))
+    print(np.allclose(cut_all_np, cut_chunked_np))
+
+    # import matplotlib.pyplot as plt
+    # # plt.imshow(all_np)
+    # plt.imshow(cut_all_np)
+    # plt.title('all_np')
+    # plt.show()
+    # # plt.imshow(chunked_np)
+    # plt.imshow(cut_chunked_np)
+    # plt.title('chunked_np')
+    # plt.show()
