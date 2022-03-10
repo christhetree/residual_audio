716fd3ad61c84e3728a5fe8dc0da197de7ca7697
christhetree <christhetree@gmail.com>
2022-03-10 14:27:27 +0900

    [cm] Implement crossfading

--- python/realtime_stft.py
+++ python/realtime_stft.py
@@ -7,7 +7,7 @@ import soundfile as sf
 import torch as tr
 from torch import Tensor as T
 from torch import nn
-from torchaudio.transforms import Spectrogram, InverseSpectrogram
+from torchaudio.transforms import Spectrogram, InverseSpectrogram, GriffinLim
 from tqdm import tqdm
 
 from python.config import OUT_DIR
@@ -26,25 +26,28 @@ class RealtimeSTFT(nn.Module):
                  n_fft: int = 2048,
                  hop_length: int = 512,
                  model_io_n_frames: int = 16,
-                 crossfade_n_frames: int = 1,
+                 crossfade_n_samples: int = 0,
                  eps: float = 1e-7) -> None:
         super().__init__()
         assert io_n_samples >= hop_length
         assert io_n_samples % hop_length == 0
         assert n_fft % 2 == 0
         assert (n_fft // 2) % hop_length == 0
+        assert crossfade_n_samples <= io_n_samples
         self.batch_size = batch_size
         self.io_n_samples = io_n_samples
         self.n_fft = n_fft
         self.hop_length = hop_length
         self.model_io_n_frames = model_io_n_frames
-        self.crossfade_n_frames = crossfade_n_frames
+        self.fade_n_samples = crossfade_n_samples
         self.eps = eps
 
         self.io_n_frames = self.io_n_samples // self.hop_length
         self.overlap_n_frames = self.n_fft // 2 // self.hop_length
         self.in_buf_n_frames = (self.overlap_n_frames * 2) - 1 + self.io_n_frames
         self.n_bins = (self.n_fft // 2) + 1
+        self.out_buf_n_samples = self.io_n_samples + self.fade_n_samples
+        assert self.out_buf_n_samples <= (self.in_buf_n_frames - 1) * self.hop_length
 
         self.stft = Spectrogram(n_fft=self.n_fft,
                                 hop_length=self.hop_length,
@@ -58,23 +61,43 @@ class RealtimeSTFT(nn.Module):
                                         pad=0,
                                         center=True,
                                         normalized=False)
-        self.in_buf = None
-        self.tmp_in_buf = None  # Prevents memory allocation
-        self.frames_buf = None
-        self.tmp_frames_buf = None  # Prevents memory allocation
-        self.reset()
+        self.gl = GriffinLim(n_fft=self.n_fft,
+                             hop_length=self.hop_length,
+                             power=2.0,
+                             n_iter=4)
 
-    def reset(self) -> None:
         self.in_buf = tr.full(
             (self.batch_size, self.in_buf_n_frames * self.hop_length),
             self.eps,
         )
         self.tmp_in_buf = tr.clone(self.in_buf)
+        self.frames_buf = None
+        self.tmp_frames_buf = None
+        self.out_buf = tr.full(
+            (self.batch_size, self.out_buf_n_samples),
+            self.eps,
+        )
+        self.fade_up = None
+        self.fade_down = None
+        if self.fade_n_samples > 0:
+            self.fade_up = tr.linspace(0, 1, self.fade_n_samples)
+            self.fade_down = tr.linspace(1, 0, self.fade_n_samples)
+        self.reset()
+
+    @property
+    def min_delay_samples(self) -> int:
+        return self.fade_n_samples
+
+    def reset(self) -> None:
+        self.in_buf.fill_(self.eps)
+        self.tmp_in_buf.fill_(self.eps)
+        # TODO(christhetree): prevent this memory allocation
         self.frames_buf = self.stft(tr.full(
             (self.batch_size, (self.model_io_n_frames * self.hop_length) - 1),
             self.eps)
         )
         self.tmp_frames_buf = tr.clone(self.frames_buf)
+        self.out_buf.fill_(self.eps)
 
     def audio_to_spec(self, audio: T) -> T:
         assert audio.shape == (self.batch_size, self.io_n_samples)
@@ -104,11 +127,36 @@ class RealtimeSTFT(nn.Module):
                               self.model_io_n_frames)
         spec = spec[:, :, -self.in_buf_n_frames:]
         rec_audio = self.istft(spec)
-        rec_audio = rec_audio[:, -self.io_n_samples:]
-        return rec_audio
+        rec_audio = rec_audio[:, -self.out_buf_n_samples:]
+        if self.fade_n_samples == 0:
+            return rec_audio
+
+        self.out_buf[:, -self.fade_n_samples:] *= self.fade_down
+        rec_audio[:, :self.fade_n_samples] *= self.fade_up
+        rec_audio[:, :self.fade_n_samples] += self.out_buf[:, -self.fade_n_samples:]
+        audio_out = rec_audio[:, :self.io_n_samples]
+        self.out_buf = rec_audio
+        return audio_out
+
+    def flush(self) -> T:
+        # TODO(christhetree): prevent this memory allocation
+        audio_out = tr.full((self.batch_size, self.io_n_samples), self.eps)
+        if self.fade_n_samples > 0:
+            audio_out[:, :self.fade_n_samples] = self.out_buf[:, -self.fade_n_samples:]
+        return audio_out
 
     def forward(self, audio: T) -> (T, T):
         spec = self.audio_to_spec(audio)
+        # derp = tr.zeros_like(spec)
+        # mag = tr.abs(spec)
+        # tr.abs(spec, out=derp)
+        # phase = tr.angle(spec)
+        # rec_spec = tr.polar(mag, phase)
+        # spec.real = tr.tensor(0.0)
+        # spec.imag = tr.tensor(0.0)
+        # spec = spec.abs()
+        # cspec = tr.complex(spec, tr.zeros_like(spec))
+        # rec_audio = self.spec_to_audio(cspec)
         rec_audio = self.spec_to_audio(spec)
         return spec, rec_audio
 
@@ -120,6 +168,7 @@ def process_file(path: str, rts: RealtimeSTFT, sr: int = SR) -> None:
     audio_pt = tr.tensor(audio_in).unsqueeze(dim=0)
 
     audio_out = []
+    rts.reset()
     for idx in tqdm(range(n_steps)):
         start_idx = idx * rts.io_n_samples
         chunk_in = audio_pt[:, start_idx:start_idx + io_n_samples]
@@ -127,6 +176,7 @@ def process_file(path: str, rts: RealtimeSTFT, sr: int = SR) -> None:
         audio_chunk = chunk_out[0].numpy()
         audio_out.append(audio_chunk)
 
+    audio_out.append(rts.flush()[0].numpy())
     audio_out = np.concatenate(audio_out)
 
     wav_name = os.path.basename(path)[:-4]
@@ -143,11 +193,18 @@ def process_file(path: str, rts: RealtimeSTFT, sr: int = SR) -> None:
 if __name__ == '__main__':
     batch_size = 1
     hop_length = 512
-    io_n_samples = 2048
+    io_n_samples = 512
     n_fft = 2048
     model_io_n_frames = 16
+    crossfade_n_samples = 64
     rts = RealtimeSTFT(
-        batch_size, io_n_samples, n_fft, hop_length, model_io_n_frames)
+        batch_size,
+        io_n_samples,
+        n_fft,
+        hop_length,
+        model_io_n_frames,
+        crossfade_n_samples
+    )
 
     audio_dir = '/Users/puntland/local_christhetree/qosmo/residual_audio/data/raw_eval'
     audio_paths = [os.path.join(audio_dir, _) for _ in os.listdir(audio_dir) 
