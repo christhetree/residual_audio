89f0b02e2f4f5cf6e1b6cad97318e3595637d285
christhetree <christhetree@gmail.com>
2022-03-24 16:59:52 +0900

    [cm] Add dry/wet and spec diff

--- python/modeling.py
+++ python/modeling.py
@@ -5,6 +5,8 @@ from typing import Tuple
 import torch as tr
 from torch import Tensor as T, nn
 
+from config import N_FFT
+
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
@@ -12,8 +14,8 @@ log.setLevel(level=os.environ.get('LOGLEVEL', 'INFO'))
 
 class SpecCNN1D(nn.Module):
     def __init__(self,
-                 n_channels: int = 1025,
-                 n_filters: int = 128,
+                 n_channels: int = (N_FFT // 2) + 1,
+                 n_filters: int = 4,
                  kernel: Tuple[int] = (5,),
                  pooling: Tuple[int] = (2,),
                  activation: nn.Module = nn.ELU()) -> None:
@@ -22,16 +24,15 @@ class SpecCNN1D(nn.Module):
         self.enc = nn.Sequential(
             nn.Conv1d(n_channels, n_filters, kernel, stride=pooling, padding=padding),
             activation,
-            nn.Conv1d(n_filters, 2 * n_filters, kernel, stride=pooling, padding=padding),
+            nn.Conv1d(n_filters, n_filters, kernel, stride=pooling, padding=padding),
             activation,
         )
         self.dec = nn.Sequential(
-            nn.ConvTranspose1d(2 * n_filters, n_filters, kernel, stride=pooling, padding=padding, output_padding=(1,)),
+            nn.ConvTranspose1d(n_filters, n_filters, kernel, stride=pooling, padding=padding, output_padding=(1,)),
             activation,
             nn.ConvTranspose1d(n_filters, n_filters, kernel, stride=pooling, padding=padding, output_padding=(1,)),
             activation,
             nn.ConvTranspose1d(n_filters, n_channels, kernel, stride=(1,), padding=padding),
-            # activation,
         )
 
     def forward(self, spec: T) -> T:
@@ -40,7 +41,7 @@ class SpecCNN1D(nn.Module):
         return rec
 
 
-class SpecCNN2D(nn.Module):
+class SpecCNN2DSmall(nn.Module):
     def __init__(self,
                  n_filters: int = 4,
                  kernel: Tuple[int] = (5, 5),
@@ -51,15 +52,10 @@ class SpecCNN2D(nn.Module):
         self.enc = nn.Sequential(
             nn.Conv2d(1, n_filters, kernel, stride=pooling, padding=padding),
             activation,
-            # nn.Conv2d(n_filters, n_filters, kernel, stride=pooling, padding=padding),
-            # activation,
-            # nn.MaxPool2d(kernel, stride=(2, 2), padding=padding),
             nn.Conv2d(n_filters, n_filters * 4, kernel, stride=pooling, padding=padding),
             activation,
-            # nn.MaxPool2d(kernel, stride=(2, 2), padding=padding),
             nn.Conv2d(n_filters * 4, n_filters * 16, kernel, stride=pooling, padding=padding),
             activation,
-            # nn.MaxPool2d(kernel, stride=(2, 2), padding=padding),
         )
         self.dec = nn.Sequential(
             nn.ConvTranspose2d(n_filters * 16, n_filters * 4, kernel, stride=pooling, padding=padding, output_padding=(0, 1)),
@@ -77,6 +73,70 @@ class SpecCNN2D(nn.Module):
         rec = self.dec(z)
         rec = rec.squeeze(1)
         return rec
+
+
+class SpecCNN2DLarge(SpecCNN2DSmall):
+    def __init__(self,
+                 n_filters: int = 16,
+                 kernel: Tuple[int] = (5, 3),
+                 pooling: Tuple[int] = (4, 1),
+                 activation: nn.Module = nn.LeakyReLU(0.1)) -> None:
+        super().__init__()
+        padding = (kernel[0] // 2, kernel[1] // 2)
+        self.enc = nn.Sequential(
+            nn.Conv2d(1, n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+            nn.BatchNorm2d(n_filters),
+            nn.Conv2d(n_filters, 2 * n_filters, kernel, stride=(2, 2), padding=padding),
+            activation,
+            nn.BatchNorm2d(2 * n_filters),
+            nn.Conv2d(2 * n_filters, 4 * n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+            nn.BatchNorm2d(4 * n_filters),
+            nn.Conv2d(4 * n_filters, 8 * n_filters, kernel, stride=(2, 2), padding=padding),
+            activation,
+            nn.BatchNorm2d(8 * n_filters),
+            nn.Conv2d(8 * n_filters, 16 * n_filters, kernel, stride=(2, 2), padding=padding),
+            activation,
+            nn.BatchNorm2d(16 * n_filters),
+        )
+        self.dec = nn.Sequential(
+            nn.ConvTranspose2d(16 * n_filters, 8 * n_filters, kernel, stride=(2, 2), padding=padding, output_padding=(0, 1)),
+            activation,
+            nn.BatchNorm2d(8 * n_filters),
+            nn.ConvTranspose2d(8 * n_filters, 4 * n_filters, kernel, stride=(2, 2), padding=padding, output_padding=(0, 1)),
+            activation,
+            nn.BatchNorm2d(4 * n_filters),
+            nn.ConvTranspose2d(4 * n_filters, 2 * n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+            nn.BatchNorm2d(2 * n_filters),
+            nn.ConvTranspose2d(2 * n_filters, n_filters, kernel, stride=(2, 2), padding=padding, output_padding=(0, 1)),
+            activation,
+            nn.BatchNorm2d(n_filters),
+            nn.ConvTranspose2d(n_filters, n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+            nn.BatchNorm2d(n_filters),
+            nn.ConvTranspose2d(n_filters, 1, kernel, stride=(1, 1), padding=padding),
+        )
+
+
+class SpecCNN2DMinimal(SpecCNN2DSmall):
+    def __init__(self,
+                 n_filters: int = 8,
+                 kernel: Tuple[int] = (5, 5),
+                 pooling: Tuple[int] = (2, 1),
+                 activation: nn.Module = nn.ELU()) -> None:
+        super().__init__()
+        padding = (kernel[0] // 2, kernel[1] // 2)
+        self.enc = nn.Sequential(
+            nn.Conv2d(1, n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+        )
+        self.dec = nn.Sequential(
+            nn.ConvTranspose2d(n_filters, n_filters, kernel, stride=pooling, padding=padding),
+            activation,
+            nn.ConvTranspose2d(n_filters, 1, kernel, stride=(1, 1), padding=padding),
+        )
 
 
 class SpecMLP(nn.Module):
