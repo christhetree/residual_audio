e01653e34ab9960c3096e87f7ffa543b8e0671f0
christhetree <christhetree@gmail.com>
2022-03-28 00:25:06 +0900

    [cm] Add fx logic

--- python/pl_wrapper.py
+++ python/pl_wrapper.py
@@ -2,6 +2,7 @@ import logging
 import os
 from typing import Optional, List, Any, Dict
 
+import torch as tr
 from pytorch_lightning import LightningModule
 from torch import Tensor as T, nn
 from torch.optim import Adam, Optimizer
@@ -25,25 +26,30 @@ class PLWrapper(LightningModule):
         self.init_lr = init_lr
         self.mae = nn.L1Loss(reduction='mean')
 
-    def forward(self, audio: T) -> (T, T):
-        spec = self.rts.audio_to_spec_offline(audio).detach()
-        rec = self.model(spec)
-        return spec, rec
+    def forward(self, batch: List[T]) -> (T, T):
+        audio_in = batch[0]
+        audio_out = batch[1]
+        spec_in = self.rts.audio_to_spec_offline(audio_in).detach()
+        rec = self.model(spec_in)
+        spec_out = spec_in
+        if not tr.allclose(audio_in, audio_out):  # TODO(christhetree)
+            spec_out = self.rts.audio_to_spec_offline(audio_out).detach()
+        return spec_out, rec
 
-    def _step(self, audio: T, prefix: str) -> T:
-        spec, rec = self.forward(audio)
-        mae_loss = self.mae(rec, spec)
+    def _step(self, batch: List[T], prefix: str) -> T:
+        spec_out, rec = self.forward(batch)
+        mae_loss = self.mae(rec, spec_out)
         loss = mae_loss
         self.log(f'{prefix}_loss', loss, prog_bar=True)
         return loss
 
     def training_step(self,
-                      batch: T,
+                      batch: List[T],
                       batch_idx: Optional[int] = None) -> T:
         return self._step(batch, 'train')
 
     def validation_step(self,
-                        batch: T,
+                        batch: List[T],
                         batch_idx: Optional[int] = None) -> T:
         return self._step(batch, 'val')
 
